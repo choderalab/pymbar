785e2beedc7890cf1f5df83bbe7b8abfd14dcaf9
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-09-09 18:05:30 -0400

    Added pip install pandas to fix version issue, hopefully.

--- src/pymbar/tests/test_mbar_harmonic_oscillators.py
+++ src/pymbar/tests/test_mbar_harmonic_oscillators.py
@@ -15,21 +15,21 @@ k_k = np.array([1.0, 1.5, 2.0])
 N_k = np.array([50, 60, 70])
         
 def test_analytical_harmonic_oscillators():
-    """Harmonic Oscillator Test: generate test object and calculate analytical results."""
+    """Harmonic Oscillators Test: generate test object and calculate analytical results."""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     mu = test.analytical_means()
     variance = test.analytical_variances()
     f_k = test.analytical_free_energies()
 
 def test_harmonic_oscillators_samples():
-    """Harmonic Oscillator Test:  draw samples via test object."""
+    """Harmonic Oscillators Test:  draw samples via test object."""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     x_n, u_kn, origin = test.sample(np.array([5, 6, 7]))
     x_n, u_kn, origin = test.sample(np.array([5, 5, 5]))
     x_n, u_kn, origin = test.sample(np.array([1., 1, 1.]))
     
 def test_shape_conversion_harmonic_oscillators():
-    """Harmonic Oscillator Test: convert shape from u_kn to u_ijn."""
+    """Harmonic Oscillators Test: convert shape from u_kn to u_ijn."""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     x_n, u_kn, origin = test.sample(np.array([5, 6, 7]))
     u_ijn, N_k = convert_ukn_to_uijn(u_kn)
@@ -39,7 +39,7 @@ def test_shape_conversion_harmonic_oscillators():
     u_ijn, N_k = convert_ukn_to_uijn(u_kn)
     
 def test_harmonic_oscillators_mbar_free_energies():
-    """Harmonic Oscillator Test: can MBAR calculate correct free energy differences?"""
+    """Harmonic Oscillators Test: can MBAR calculate correct free energy differences?"""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     x_n, u_kn, origin = test.sample(N_k)
     u_ijn, N_k_output = convert_ukn_to_uijn(u_kn)
@@ -57,7 +57,7 @@ def test_harmonic_oscillators_mbar_free_energies():
     eq(z / 3.0, np.zeros(len(z)), decimal=0)
 
 def test_exponential_mbar__xkn():
-    """Harmonic Oscillator Test: can MBAR calculate E(x_kn)??"""
+    """Harmonic Oscillators Test: can MBAR calculate E(x_kn)??"""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     x_n, u_kn, origin = test.sample(N_k)
     u_ijn, N_k_output = convert_ukn_to_uijn(u_kn)
@@ -78,7 +78,7 @@ def test_exponential_mbar__xkn():
     eq(z / 3.0, np.zeros(len(z)), decimal=0)
 
 def test_exponential_mbar_xkn_squared():
-    """Harmonic Oscillator Test: can MBAR calculate E(x_kn^2)??"""
+    """Harmonic Oscillators Test: can MBAR calculate E(x_kn^2)??"""
     test = harmonic_oscillators.HarmonicOscillatorsTestCase(O_k, k_k)
     x_n, u_kn, origin = test.sample(N_k)
     u_ijn, N_k_output = convert_ukn_to_uijn(u_kn)
