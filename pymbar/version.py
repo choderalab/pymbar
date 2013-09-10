85e70aab263a04541bc6f5f204189ce5cab6902e
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-09-10 18:23:22 -0400

    Added version.py and updated setup.py to use it.

---
+++ pymbar/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pymbar setup.
+short_version = '2.0.0'
+version = '2.0.0'
+full_version = '2.0.0.dev-6ce3f7b'
+git_revision = '6ce3f7be10c4098aa408135b9d3c4da528a9837b'
+release = False
+
+if not release:
+    version = full_version
--- setup.py
+++ setup.py
@@ -5,7 +5,80 @@ from distutils.sysconfig import get_config_var
 from distutils.core import setup, Extension
 import numpy
 import glob
+import os
+import subprocess
 
+##########################
+VERSION = "2.0.0"
+ISRELEASED = False
+__version__ = VERSION
+##########################
+
+################################################################################
+# Writing version control information to the module
+################################################################################
+
+def git_version():
+    # Return the git revision as a string
+    # copied from numpy setup.py
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+
+    return GIT_REVISION
+
+
+def write_version_py(filename='pymbar/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM pymbar setup.
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+"""
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of numpy.version messes up the build under Python 3.
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev-' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+write_version_py()
 CMBAR = Extension('_pymbar',
                   sources = ["pymbar/_pymbar.c"],
                   extra_compile_args=["-std=c99","-O2","-shared","-msse2","-msse3"],
