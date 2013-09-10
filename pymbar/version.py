1c1eb383a434286d127c948e8cac2457db5680b7
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-09-10 19:27:15 -0400

    Minor fix to setup.py

--- pymbar/version.py
+++ pymbar/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM pymbar setup.
 short_version = '2.0.0'
 version = '2.0.0'
-full_version = '2.0.0.dev-2abaec8'
-git_revision = '2abaec8747337775d35bb93a4f2606e2cdbeddb8'
+full_version = '2.0.0.dev-13df6d0'
+git_revision = '13df6d046eaa1d4ed8952e1426f991f1c238b76a'
 release = False
 
 if not release:
