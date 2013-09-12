b1832344e6613ecc8fbac9b42dfb079274ac75d6
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-09-12 10:17:34 -0400

    Updated copyright again.

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pymbar'
-copyright = u'2006-2013 pymbar developerment team'
+copyright = u'2006-2013 pymbar developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
