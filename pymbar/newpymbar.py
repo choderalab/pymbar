0cf32b0165b53cd62aa2f63a7efc8ec17a9370ee
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-09-12 12:50:08 -0400

    Temporary checkin with minor improvements to iterative code

--- pymbar/newpymbar.py
+++ pymbar/newpymbar.py
@@ -14,6 +14,17 @@
 # You should have received a copy of the GNU General Public License along with
 # mdtraj. If not, see http://www.gnu.org/licenses/.
 
+"""
+
+Notes
+-----
+
+If numerical precision / underflow is a problem, the following code could
+be improved by replacing `logsumexp` with an implementation of `logsumexp`
+that uses Kahan summation--as in `math.fsum()`.
+
+"""
+
 import numpy as np
 from mdtraj.utils import ensure_type
 from sklearn.utils.extmath import logsumexp
@@ -35,6 +46,9 @@ class MBAR(object):
 
         self.u_ki = ensure_type(u_ki, np.float64, 2, 'u_ki')
         
+        self.q_ki = np.exp(-self.u_ki)
+        self.q_ki /= self.q_ki.max(0)  # Divide for overflow.
+        
         self.N_k = ensure_type(N_k, np.float64, 1, 'N_k', (self.n_states))
         self.log_N_k = np.log(self.N_k)
         
@@ -42,36 +56,15 @@ class MBAR(object):
         
         self.states = self.u_ki
 
-    def _calc_s(self, c):
-        return self.r_ki.dot(c ** -1.)
+    def self_consistent_eqn_fast(self, f_i):
+        c_i = np.exp(f_i)
+        denom_n = self.q_ki.T.dot(self.N_k * c_i)
         
-    def _calc_c(self, s):
-        return self.q_ki.dot(s ** -1.)
+        num = self.q_ki.dot(denom_n ** -1.)
+        
+        new_f_i = 1.0 * np.log(num)
 
-    def _solve_iterative_c(self, max_iter=10000, tol=1E-12):
-        """Solve the MBAR equations in 'c' space.
-        
-        Notes
-        -----
-        WARNING: this may lead to precision issues.
-        BUT: this is really fast.
-        """
-    
-        self.q_ki = np.exp(-self.u_ki)
-        self.q_ki /= self.q_ki.max(0)  # Divide for overflow.
-        self.r_ki = self.q_ki.T * self.N_k    
-        
-        c = np.ones(self.n_states)
-        c_old = c.copy()
-        for k in xrange(max_iter):
-            s = self._calc_s(c)
-            c = self._calc_c(s)
-            epsilon = np.linalg.norm(np.log(c / c_old))
-            if epsilon <= tol:
-                break
-            c_old = c.copy()
-            print(c[0])
-        return c
+        return f_i + new_f_i
 
     def self_consistent_eqn(self, f_i):
       
@@ -81,7 +74,13 @@ class MBAR(object):
         exp_args = -L_n - self.u_ki
         q_i = logsumexp(exp_args, axis=1)
         
-        return  f_i + q_i
+        return f_i + q_i
+
+    def fixed_point_eqn(self, f_i):
+        return self.self_consistent_eqn(f_i) + f_i
+
+    def fixed_point_eqn_fast(self, f_i):
+        return self.self_consistent_eqn_fast(f_i) + f_i
 
     def solve_iterative(self, max_iter=10000, tol=1E-12):
         """Iteratively solve the MBAR equations in 'f' space."""
@@ -120,3 +119,25 @@ class MBAR(object):
         start = np.zeros(self.n_states)
         f, final_objective, convergence_parms = scipy.optimize.fmin_l_bfgs_b(self.objective, start, self.gradient, factr=1E-2, pgtol=1E-8)
         return f
+
+
+    def objective_fast(self, f_i):
+        F = self.N_k.dot(f_i)
+
+        c_i = np.exp(f_i)
+
+        log_arg = self.q_ki.T.dot(self.N_k * c_i)
+        F -= np.log(log_arg).sum()
+        return F * -1.
+        
+
+    def gradient_fast(self, f_i):   
+        c_i = np.exp(f_i)
+        denom_n = self.q_ki.T.dot(self.N_k * c_i)
+        
+        num = self.q_ki.dot(denom_n ** -1.)
+
+        grad = self.N_k * (1.0 - c_i * num)
+        grad *= -1.
+
+        return grad
