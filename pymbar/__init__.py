ff8dc7a52fb9c7922822c5bb939679a21cfa57b0
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-04-22 16:52:14 -0400

    Switched import style for version import

--- pymbar/__init__.py
+++ pymbar/__init__.py
@@ -20,7 +20,7 @@ from pymbar.exponential_averaging import EXP, EXPGauss
 import pymbar.timeseries
 import pymbar.testsystems
 import pymbar.confidenceintervals
-import pymbar.version
+from mdtraj import version
 
 __all__ = ['EXP', 'EXPGauss', 'BAR', 'BARzero', 'MBAR', 'timeseries', 'testsystems', 'confidenceintervals', 'utils']
 
