b28eabe23438a497f1f31697a0411b150c674d7e
Stefano Martiniani <stefano.martiniani@gmail.com>
2015-01-26 17:14:06 +0000

    change default value of memsage keyword

--- pymbar/timeseries.py
+++ pymbar/timeseries.py
@@ -775,7 +775,7 @@ def detectEquilibration(A_t, fast=True, nskip=1):
     return (t, g, Neff_max)
 
 
-def statisticalInefficiency_fft(A_n, mintime=3, memsafe=True):
+def statisticalInefficiency_fft(A_n, mintime=3, memsafe=False):
     """Compute the (cross) statistical inefficiency of (two) timeseries.
 
     Parameters
@@ -787,10 +787,10 @@ def statisticalInefficiency_fft(A_n, mintime=3, memsafe=True):
         The algorithm terminates after computing the correlation time out to mintime when the
         correlation function furst goes negative.  Note that this time may need to be increased
         if there is a strong initial negative peak in the correlation function.
-    memsafe: bool, optional, default=True
-        clear np.fft cache to avoid a fast increase in memory consumption when this function
-        is called on many arrays of different sizes. If this function is used several times on
-        arrays of comparable size then one might benefit from setting this option to False.
+    memsafe: bool, optional, default=False
+        If this function is used several times on arrays of comparable size then one might benefit 
+        from setting this option to False. If set to True then clear np.fft cache to avoid a fast 
+        increase in memory consumption when this function is called on many arrays of different sizes.
     Returns
     -------
     g : np.ndarray,
@@ -888,7 +888,7 @@ def detectEquilibration_binary_search(A_t, bs_nodes=10):
         
         for k, t in enumerate(time_grid):
             if t < T-1:
-                g_t[k] = statisticalInefficiency_fft(A_t[t:])
+                g_t[k] = statisticalInefficiency_fft(A_t[t:], memsafe=True)
                 Neff_t[k] = (T - t + 1) / g_t[k]
 
         Neff_max = Neff_t.max()
