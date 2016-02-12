e40536f3a7e5a1209f6401db4e72baeeb0d2749f
Levi Naden <ln8dc@virginia.edu>
2016-02-12 12:23:26 -0500

    Cleaned up the output of the warning messages that are thrown in from the Scipy solvers and the response to them

--- pymbar/mbar_solvers.py
+++ pymbar/mbar_solvers.py
@@ -327,14 +327,12 @@ def solve_mbar_once(u_kn_nonzero, N_k_nonzero, f_k_nonzero, method="hybr", tol=1
     #If there were runtime warnings, show the messages
     if len(w) > 0:
         for warn_msg in w:
-            warnings.showwarning(warn_msg.message, warn_msg.category, warn_msg.filename, warn_msg.lineno, warn_msg.file, warn_msg.line) 
+            warnings.showwarning(warn_msg.message, warn_msg.category, warn_msg.filename, warn_msg.lineno, warn_msg.file, "") 
         #Ensure MBAR solved correctly
         W_nk_check = mbar_W_nk(u_kn_nonzero, N_k_nonzero, f_k_nonzero)
         check_w_normalized(W_nk_check, N_k_nonzero)
-        msg = "MBAR weights converged within tolerance, despite the SciPy Warnings. Please validate your results."
-        #Minimize the amount of output from our warning.
-        #                    msg,           Type, filename, linenumber, file, line 
-        warnings.showwarning(msg, RuntimeWarning,       '',         '', None,   '')
+        print "MBAR weights converged within tolerance, despite the SciPy Warnings. Please validate your results."
+       
             
     return f_k_nonzero, results
 
