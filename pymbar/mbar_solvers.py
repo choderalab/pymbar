6da0f40b20c834b7c0f132bf26e04bc5e76b4a09
Levi Naden <ln8dc@virginia.edu>
2016-02-12 12:31:20 -0500

    Fixed the print statement for Python 3.x

--- pymbar/mbar_solvers.py
+++ pymbar/mbar_solvers.py
@@ -331,7 +331,7 @@ def solve_mbar_once(u_kn_nonzero, N_k_nonzero, f_k_nonzero, method="hybr", tol=1
         #Ensure MBAR solved correctly
         W_nk_check = mbar_W_nk(u_kn_nonzero, N_k_nonzero, f_k_nonzero)
         check_w_normalized(W_nk_check, N_k_nonzero)
-        print "MBAR weights converged within tolerance, despite the SciPy Warnings. Please validate your results."
+        print("MBAR weights converged within tolerance, despite the SciPy Warnings. Please validate your results.")
        
             
     return f_k_nonzero, results
