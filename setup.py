aefdd73c63fcad10b76cf360bb235ea27a41f216
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-12-31 11:57:36 -0500

    Add 'six' to setup.py requirements; add travis tests for pip install

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ def buildKeywordDictionary():
     # setupKeywords["test_suite"]        = "tests" # requires we migrate to setuptools
     setupKeywords["platforms"]         = ["Linux", "Mac OS X", "Windows"]
     setupKeywords["description"]       = "Python implementation of the multistate Bennett acceptance ratio (MBAR) method."
-    setupKeywords["requires"]          = ["numpy", "scipy", "nose", "numexpr"]
+    setupKeywords["requires"]          = ["numpy", "scipy", "nose", "numexpr", "six"]
     setupKeywords["long_description"]  = """
     Pymbar (https://simtk.org/home/pymbar) is a library
     that provides tools for optimally combining simulations
