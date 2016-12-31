5228ac6804ef7a8e58c963c138abb2294d813098
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-12-31 11:40:51 -0500

    Remove pymbar-datasets references from __init__.py

--- pymbar/testsystems/__init__.py
+++ pymbar/testsystems/__init__.py
@@ -4,4 +4,3 @@ from pymbar.testsystems.harmonic_oscillators import HarmonicOscillatorsTestCase
 from pymbar.testsystems.exponential_distributions import ExponentialTestCase
 from pymbar.testsystems.timeseries import correlated_timeseries_example
 from pymbar.testsystems.gaussian_work import gaussian_work_example
-from pymbar.testsystems.pymbar_datasets import load_gas_data, load_8proteins_data
