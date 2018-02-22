edf95c915da4029808f892cb2857ee69c517ece8
Michael Shirts <mrshirts@gmail.com>
2018-02-22 16:53:50 -0700

    Updated README.md, plus py2 compatibility.

--- examples/heat-capacity/heat-capacity.py
+++ examples/heat-capacity/heat-capacity.py
@@ -4,6 +4,8 @@
 #=========================================================
 #IMPORTS
 #=========================================================
+
+from __future__ import print_function
 import sys
 import numpy
 import pymbar # for MBAR analysis
