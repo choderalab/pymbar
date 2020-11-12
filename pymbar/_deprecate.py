c75539e5bb49c257ae37f119f6ecc27d9c10fd0a
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-11-12 09:42:49 +0100

    Update deprecated wrapper

--- pymbar/_deprecate.py
+++ pymbar/_deprecate.py
@@ -10,6 +10,7 @@ It may be removed in future versions. Please update your code to use `{}` instea
 
 _deprecated_names = {}
 
+
 def _deprecate(fn, old_name):
     """
     Helper to deprecate old names. It will add a deprecation warning and update the
@@ -20,14 +21,21 @@ def _deprecate(fn, old_name):
     fn : callable
         The new function that will replace the old one
     old_name : str
-        Original name that to be deprecated
+        Original name to be deprecated
 
     >>> OldName = _deprecate(new_name, "OldName")
     """
     _deprecated_names[old_name] = fn.__name__
+
     @wraps(fn)
     def deprecated(*args, **kwargs):
-        warn(f"`{old_name}` will be deprecated in pymbar 4+. Please update your code to use `{fn.__name__}` instead.", DeprecationWarning, stacklevel=2)
+        warn(
+            f"`{old_name}` will be deprecated in pymbar 4+. Please update your code to use `{fn.__name__}` instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
         return fn(*args, **kwargs)
+
+    fn_doc = fn.__doc__ or ""
     deprecated.__doc__ = fn.__doc__ + _docstring_warning.format(fn.__name__)
     return deprecated
