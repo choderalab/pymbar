fixes to standard deviation.

pymbar/fes.py
@@ -1416,8 +1416,8 @@ class FES:
                 fall = np.zeros([len(histogram_data["f"]), n_bootstraps])
                 for b in range(n_bootstraps):
                     h = histogram_datas[b]  # just to make this shorter
-                    fall[:, b] = h["f"] - h["f"][j]
-                df_i = np.std(fall, axis=1)
+                    fall[:, b] = h["f"] - h["f"][j] # subtract out the reference bin
+                df_i = np.std(fall, ddof=1, axis=1)
 
         elif reference_point == "from-normalization":
             # Determine uncertainties from normalization that \sum_i p_i = 1.
@@ -1565,10 +1565,12 @@ class FES:
 
         if reference_point == "from-lowest":
             fmin = np.min(f_i)
-            f_i = f_i - fmin
+            f_i = f_i - fmin            
+            wheremin = np.argmin(f_i)  # needed for uncertainties, as we zero by location, not values
         elif reference_point == "from-specified":
             fmin = -self.kde.score_samples(np.array(fes_reference).reshape(1, -1))
             f_i = f_i - fmin
+            wheremin = p.argmin(f_i) # needed for uncertainties, as we zero by location, not values
         elif reference_point == "from-normalization":
             # uncertainites "from normalization" reference is already applied, since
             # the density is normalized.
@@ -1595,7 +1597,8 @@ class FES:
 
             fall = np.zeros([len(x), n_bootstraps])
             for b in range(n_bootstraps):
-                fall[:, b] = -self.kdes[b].score_samples(x) - fmin
+                fall[:, b] = -self.kdes[b].score_samples(x)
+                fall[:, b] -= fall[wheremin, b]
             df_i = np.std(fall, ddof=1, axis=1)
         else:
             raise ParameterError(