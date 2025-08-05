a16f420409dba288c5843a642ced72d163f77673
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2025-08-05 11:35:16 -0700

    migrate to setuptools_scm and use a pyproject.toml file

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
-import versioneer
 
 
 long_description = """
-Pymbar (https://simtk.org/home/pymbar) is a library
+Pymbar (https://github.com/choderalab/pymbar) is a library
 that provides tools for optimally combining simulations
 from multiple thermodynamic states using maximum likelihood
 methods to compute free energies (normalization constants)
@@ -21,10 +20,8 @@ setup(
     packages=find_packages(),
     long_description=long_description[1:],
     classifiers=["License :: OSI Approved :: MIT License", "Programming Language :: Python :: 3"],
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    python_requires=">=3.6",
-    install_requires=["numpy>=1.12",
+    python_requires=">=3.10",
+    install_requires=["numpy>=1.17",
                       "scipy",
                       "numexpr",
                       ],
